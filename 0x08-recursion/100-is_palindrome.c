40a87d687cbc4009980fd50ed937fc314a37f205
Your Name <you@example.com>
2023-04-05 19:32:42 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
@@ -10,6 +27,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
+
 	return (1 + _strlen_recursion(s + 1));
 }
 
@@ -25,20 +43,9 @@ int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
 		return (0);
+
 	if (i >= len)
 		return (1);
-	return (check_pal(s, i + 1, len - 1));
-}
 
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
- *
- * Return: 1 if it is, 0 it's not
- */
-int is_palindrome(char *s)
-{
-	if (*s == '\0')
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+	return (check_pal(s, i + 1, len - 1));
 }
