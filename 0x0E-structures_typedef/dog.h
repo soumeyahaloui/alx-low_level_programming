d579678b311dbaaef9cf2f92357d0a994d18ced4
Your Name <you@example.com>
2023-04-17 19:14:49 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+
+dogStruct - Contains basic information about a dog
+@name: Name of the dog
+@age: Age of the dog
+@owner: Owner of the dog
+Detailed Description: Provides a detailed description of the dog struct
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
