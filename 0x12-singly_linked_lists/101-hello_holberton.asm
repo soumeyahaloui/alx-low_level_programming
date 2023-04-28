f979ca3230fe809d85417416f9a554d96eccce36
soumeyahaloui <soumeyahaloui@gmail.com>
2023-04-27 19:57:38 -0700

    updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,9 @@
-global main
-extern printf
-
+global    main
+          extern    printf
 main:
-push rbp
-mov rdi, msg
-xor rax, rax
-call printf
-pop rbp
-ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
 format: db `Hello, Holberton\n`,0
