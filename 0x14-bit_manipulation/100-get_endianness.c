32e0d6e49d53c26974fae1c31545e4eb3523abc6
soumeyahaloui <soumeyahaloui@gmail.com>
2023-05-04 18:12:20 -0700

    updated

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness of the machine
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	/* If the least significant byte of the int is 1, the machine is little endian */
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
