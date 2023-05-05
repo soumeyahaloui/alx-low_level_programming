debfaa82fdded0008fd452b3dcf70148b0cabc14
soumeyahaloui <soumeyahaloui@gmail.com>
2023-05-04 17:48:30 -0700

    updated

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,14 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask = 1;
+	int bits = sizeof(unsigned long int) * 8;
 	int i;
 
-	/* Find the highest power of 2 that is <= n */
-	while ((mask << 1) <= n)
-		mask <<= 1;
-
-	/* Print the binary representation */
-	for (i = 0; mask != 0; mask >>= 1, i++)
-		_putchar((n & mask) ? '1' : '0');
-
-	/* If n is 0, print a single 0 */
-	if (i == 0)
-		_putchar('0');
+	for (i = bits - 1; i >= 0; i--)
+	{
+		if ((n >> i) & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
 }
